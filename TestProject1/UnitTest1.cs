ed5a815ad8f76f4f2d35d70c91fa3a30d42cc225
brandon khublall <brandon.khublall@hotmail.com>
2023-04-15 19:47:16 -0400

    added view interface

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,14 +1,27 @@
+using Milestone6_Team_YourName;
 using Newtonsoft.Json.Bson;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
+using Budget;
 
 namespace TestProject1
 {
-    public class UnitTest1 
+    public class UnitTest1 : ViewInterface
     {
+        public UnitTest1()
+        {
 
-        
-       
+         
+        }
 
+        public void DisplayCatTypes(List<Category.CategoryType> categoryTypes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayList(List<Category> categories)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
